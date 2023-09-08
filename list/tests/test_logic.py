eb43990244617cbd29a4133f03bfa64254b963d3
8EAUTIFUL <pawel.wardachpr@gmail.com>
2023-09-08 13:59:38 +0200

    TestUpdates

--- list/tests/test_logic.py
+++ list/tests/test_logic.py
@@ -11,25 +11,48 @@ User = get_user_model()
 
 
 @pytest.fixture
-def create_test_data():
+def create_user():
     user = User.objects.create_user(username='testuser', password='testpassword')
+    return user
+
+
+@pytest.fixture
+def create_authenticated_user():
+    def make_authenticated_user(username, password):
+        user = User.objects.create_user(username=username, password=password)
+        client = APIClient()
+        client.login(username=username, password=password)
+        return user, client
+
+    return make_authenticated_user
+
+
+@pytest.fixture
+def create_list(create_user):
+    user = create_user
     list_obj = List.objects.create(user=user, date='2023-09-05', important_task=True)
+    return list_obj
+
+
+@pytest.fixture
+def create_task(create_list):
+    list_obj = create_list
     task = Task.objects.create(
         list=list_obj, is_completed=True, title='Sample Task', description='This is a test task', tag='Home')
-    return user, list_obj, task
+    return task
 
 
 @pytest.mark.django_db
 class TestListLogic:
-    def test_get_user_lists(self, create_test_data):
-        user, _, _ = create_test_data
+    def test_get_user_lists(self, create_user, create_list):
+        user = create_user
 
         lists = get_user_lists(user=user)
         assert len(lists) == 1
-        assert lists[0].user == user
+        assert user in [list.user for list in lists]
 
-    def test_get_list_details(self, create_test_data):
-        _, list_obj, _ = create_test_data
+    def test_get_list_details(self, create_list):
+        list_obj = create_list
 
         list_details = get_list_details(list_id=list_obj.id)
         assert list_details.id == list_obj.id
@@ -45,11 +68,10 @@ class TestListLogic:
             get_list_details(None)
 
 
-# TASKS TESTS
 @pytest.mark.django_db
 class TestTaskLogic:
-    def test_get_list_tasks(self, create_test_data):
-        _, list_obj, _ = create_test_data
+    def test_get_list_tasks(self, create_list, create_task):
+        list_obj = create_list
 
         tasks = get_list_tasks(list_id=list_obj.id)
         assert len(tasks) == 1
@@ -60,8 +82,8 @@ class TestTaskLogic:
                 break
         assert found_task is not None
 
-    def test_get_task_details(self, create_test_data):
-        _, _, task = create_test_data
+    def test_get_task_details(self, create_task):
+        task = create_task
 
         task_details = get_task_details(task_id=task.id)
         assert task_details.id == task.id
@@ -78,82 +100,179 @@ class TestTaskLogic:
 
 @pytest.mark.django_db
 class TestListPermissionsLogic:
-    def test_if_anonymous_user_post_list_returns_403(self):
+
+    def test_if_authenticated_user_get_lists_returns_200(self, create_authenticated_user):
+        user, client = create_authenticated_user(username='testuser', password='testpassword')
+        url_pattern = 'get_create_list'
+        response = client.get(reverse(url_pattern))
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_authenticated_user_create_list_returns_201(self, create_authenticated_user):
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'get_create_list'
+        data = {
+            'date': '2023-09-05',
+            'important_task': True
+        }
+
+        response = client.post(reverse(url_pattern), data=data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_if_authenticated_user_update_list_returns_200(self, create_authenticated_user, create_list):
+        list_obj = create_list
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'update_delete_list'
+        data = {
+            'date': '2023-10-09',
+            'important_task': False
+        }
+
+        response = client.put(reverse(url_pattern, kwargs={'list_id': list_obj.id}), data=data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_authenticated_user_delete_list_returns_204(self, create_authenticated_user, create_list):
+        list_obj = create_list
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'update_delete_list'
+
+        response = client.delete(reverse(url_pattern, kwargs={'list_id': list_obj.id}))
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_if_anonymous_user_get_lists_returns_403(self):
         client = APIClient()
-        url_pattern_name = 'create_list'
+        url_pattern = 'get_create_list'
+        response = client.get(reverse(url_pattern))
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_anonymous_user_create_list_returns_403(self):
+        client = APIClient()
+        url_pattern = 'get_create_list'
         data = {
             'date': '2023-09-05',
             'important_task': False
         }
 
-        response = client.post(reverse(url_pattern_name), data=data)
+        response = client.post(reverse(url_pattern), data=data)
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_anonymous_user_update_list_returns_403(self, create_test_data):
-        _, list_obj, _ = create_test_data
+    def test_if_anonymous_user_update_list_returns_403(self, create_list):
+        list_obj = create_list
         client = APIClient()
-        list_id = list_obj.id
-        url_pattern_name = 'update_delete_list'
+        url_pattern = 'update_delete_list'
         data = {
             'date': '2023-09-06',
             'important_task': False
         }
 
-        response = client.put(reverse(url_pattern_name, kwargs={'list_id': list_id}), data=data, format='json')
+        response = client.put(reverse(url_pattern, kwargs={'list_id': list_obj.id}), data=data, format='json')
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_anonymous_user_delete_list_returns_403(self, create_test_data):
-        _, list_obj, _ = create_test_data
+    def test_if_anonymous_user_delete_list_returns_403(self, create_list):
+        list_obj = create_list
         client = APIClient()
-        list_id = list_obj.id
-        url_pattern_name = 'update_delete_list'
-        response = client.delete(reverse(url_pattern_name, kwargs={'list_id': list_id}))
+        url_pattern = 'update_delete_list'
+        response = client.delete(reverse(url_pattern, kwargs={'list_id': list_obj.id}))
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    @pytest.mark.django_db
-    class TestTaskPermissionsLogic:
-        def test_if_anonymous_user_post_task_returns_403(self):
-            client = APIClient()
-            url_pattern_name = 'create_task'
 
-            data = {
-                'is_completed': False,
-                'title': 'New task',
-                'description': 'Description',
-                'tag': 'Work'
-            }
+@pytest.mark.django_db
+class TestTaskPermissionsLogic:
 
-            response = client.post(reverse(url_pattern_name, kwargs={'list_id': 1}), data=data, format='json')
+    def test_if_authenticated_user_get_tasks_returns_200(self, create_authenticated_user, create_list):
+        list_obj = create_list
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'get_create_task'
+        response = client.get(reverse(url_pattern, kwargs={'list_id': list_obj.id}))
 
-            assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response.status_code == status.HTTP_200_OK
 
-        def test_if_anonymous_user_update_task_returns_403(self, create_test_data):
-            _, list_obj, task = create_test_data
-            task_id = task.id
-            list_id = list_obj.id
-            client = APIClient()
-            url_pattern_name = 'update_delete_task'
-            data = {
-                'is_completed': True,
-                'title': 'Old Task',
-                'description': 'Second Description',
-                'tag': 'Sport'
-            }
+    def test_if_authenticated_user_create_task_returns_201(self, create_authenticated_user, create_list):
+        list_obj = create_list
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'get_create_task'
+        data = {
+            'is_completed': True,
+            'title': 'title',
+            'description': 'description',
+            'tag': 'Work'
+        }
 
-            response = client.put(reverse(url_pattern_name, kwargs={'list_id': list_id, 'task_id': task_id}), data=data)
-            assert response.status_code == status.HTTP_403_FORBIDDEN
+        response = client.post(reverse(url_pattern, kwargs={'list_id': list_obj.id}), data=data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
 
-        def test_if_anonymous_user_delete_task_returns_403(self, create_test_data):
-            _, list_obj, task = create_test_data
-            client = APIClient()
-            task_id = task.id
-            list_id = list_obj.id
-            url_pattern_name = 'update_delete_task'
+    def test_if_authenticated_user_update_task_returns_200(self, create_authenticated_user, create_list, create_task):
+        list_obj = create_list
+        task = create_task
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'update_delete_task'
+        data = {
+            'is_completed': False,
+            'title': 'title1',
+            'description': 'description1',
+            'tag': 'Home'
+        }
 
-            response = client.delete(reverse(url_pattern_name, kwargs={'list_id': list_id, 'task_id': task_id}))
-            assert response.status_code == status.HTTP_403_FORBIDDEN
+        response = client.put(
+            reverse(url_pattern, kwargs={'list_id': list_obj.id, 'task_id': task.id}), data=data, format='json')
+        assert response.status_code == status.HTTP_200_OK
 
+    def test_if_authenticated_user_delete_task_returns_204(self, create_authenticated_user, create_list, create_task):
+        list_obj = create_list
+        task = create_task
+        user, client = create_authenticated_user(username='123', password='123')
+        url_pattern = 'update_delete_task'
+        response = client.delete(reverse(url_pattern, kwargs={'list_id': list_obj.id, 'task_id': task.id}))
 
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_if_anonymous_user_get_tasks_returns_403(self, create_list):
+        list_obj = create_list
+        client = APIClient()
+        url_pattern = 'get_create_task'
+        response = client.get(reverse(url_pattern, kwargs={'list_id': list_obj.id}))
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_anonymous_user_create_task_returns_403(self):
+        client = APIClient()
+        url_pattern = 'get_create_task'
+
+        data = {
+            'is_completed': False,
+            'title': 'New task',
+            'description': 'Description',
+            'tag': 'Work'
+        }
+
+        response = client.post(reverse(url_pattern, kwargs={'list_id': 1}), data=data, format='json')
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_anonymous_user_update_task_returns_403(self, create_list, create_task):
+        list_obj = create_list
+        task = create_task
+        client = APIClient()
+        url_pattern = 'update_delete_task'
+        data = {
+            'is_completed': True,
+            'title': 'Old Task',
+            'description': 'Second Description',
+            'tag': 'Sport'
+        }
+
+        response = client.put(reverse(url_pattern, kwargs={'list_id': list_obj.id, 'task_id': task.id}), data=data)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_anonymous_user_delete_task_returns_403(self, create_list, create_task):
+        list_obj = create_list
+        task = create_task
+        client = APIClient()
+        url_pattern = 'update_delete_task'
+
+        response = client.delete(reverse(url_pattern, kwargs={'list_id': list_obj.id, 'task_id': task.id}))
+        assert response.status_code == status.HTTP_403_FORBIDDEN
--- list/urls.py
+++ list/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ListView, ListDetailView, TaskView, TaskDetailView
 
 urlpatterns = [
-    path('lists/', ListView.as_view(), name='create_list'),
+    path('lists/', ListView.as_view(), name='get_create_list'),
     path('lists/<int:list_id>/', ListDetailView.as_view(), name='update_delete_list'),
-    path('lists/<int:list_id>/tasks/', TaskView.as_view(), name='create_task'),
+    path('lists/<int:list_id>/tasks/', TaskView.as_view(), name='get_create_task'),
     path('lists/<int:list_id>/tasks/<int:task_id>/', TaskDetailView.as_view(), name='update_delete_task')
 ]
 
